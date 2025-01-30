e247c6cc97709b05dd360362f4c8d9632561b950
Daxin319 <lylehigg@gmail.com>
2025-01-30 15:36:13 -0700

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GitHub/go-pokedex
 
 go 1.23.5
+
+require github.com/Daxin319/Go-Pokedex/apiLogic v0.0.0-20250130220413-0e6bf1f5e33d
--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
+
+	apilogic "github.com/Daxin319/Go-Pokedex/apiLogic"
 )
 
 func main() {
-	config := &Config{
-		nextURL:     "",
-		previousURL: "",
+	config := &apilogic.Config{
+		NextURL:     "",
+		PreviousURL: "",
 	}
 	supportedCommands := map[string]cliCommand{
 		"exit": {
@@ -24,12 +23,12 @@ func main() {
 		"map": {
 			name:        "map",
 			description: "Displays the next of 20 regions in the Pokemon world",
-			callback:    commandMap,
+			callback:    apilogic.commandMap,
 		},
 		"mapb": {
 			name:        "mapb",
 			description: "Displays the previous 20 regions in the Pokemon world",
-			callback:    commandMapB,
+			callback:    apilogic.commandMapB,
 		},
 	}
 	supportedCommands["help"] = cliCommand{
@@ -53,23 +52,10 @@ func main() {
 	}
 }
 
-type Locations struct {
-	Count    int     `json:"count"`
-	Next     string  `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
-}
-type Config struct {
-	nextURL     string
-	previousURL string
-}
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(c *Config) error
+	callback    func(c *apilogic.Config) error
 }
 
 func cleanInput(text string) []string {
@@ -79,14 +65,14 @@ func cleanInput(text string) []string {
 
 }
 
-func commandExit(c *Config) error {
+func commandExit(c *apilogic.Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp(input map[string]cliCommand, c *Config) func(c *Config) error {
-	return func(c *Config) error {
+func commandHelp(input map[string]cliCommand, c *apilogic.Config) func(c *apilogic.Config) error {
+	return func(c *apilogic.Config) error {
 		fmt.Print("Welcome to the Pokedex!\nUsage:\n\n\n")
 
 		for _, command := range input {
@@ -95,76 +81,4 @@ func commandHelp(input map[string]cliCommand, c *Config) func(c *Config) error {
 
 		return nil
 	}
-}
-func commandMap(c *Config) error {
-	var url string
-	if c.nextURL != "" {
-		url = c.nextURL
-	} else {
-		url = "https://pokeapi.co/api/v2/location-area/"
-	}
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("error pulling location data from pokeapi")
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response body")
-	}
-	if resp.StatusCode > 299 {
-		return fmt.Errorf("response failed with error code %d and \nbody: %s", resp.StatusCode, resp.Body)
-	}
-	locations := Locations{}
-	err = json.Unmarshal(body, &locations)
-	if err != nil {
-		return fmt.Errorf("error unmarshaling json file")
-	}
-	for _, area := range locations.Results {
-		fmt.Println(area.Name)
-	}
-	if locations.Previous != nil {
-		c.previousURL = *locations.Previous
-	} else {
-		c.previousURL = url
-	}
-	c.nextURL = locations.Next
-	return nil
-}
-func commandMapB(c *Config) error {
-	var url string
-	if c.previousURL == "" || c.previousURL == "https://pokeapi.co/api/v2/location-area/" {
-		fmt.Println("You are on the first page")
-		c.nextURL = "https://pokeapi.co/api/v2/location-area/"
-		return nil
-	} else {
-		url = c.previousURL
-	}
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("error pulling location data from pokeapi")
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("error reading response body")
-	}
-	if resp.StatusCode > 299 {
-		return fmt.Errorf("response failed with error code %d and \nbody: %s", resp.StatusCode, resp.Body)
-	}
-	locations := Locations{}
-	err = json.Unmarshal(body, &locations)
-	if err != nil {
-		return fmt.Errorf("error unmarshaling json file")
-	}
-	for _, area := range locations.Results {
-		fmt.Println(area.Name)
-	}
-	c.nextURL = locations.Next
-	if locations.Previous != nil {
-		c.previousURL = *locations.Previous
-	} else {
-		c.previousURL = ""
-	}
-	return nil
 }
